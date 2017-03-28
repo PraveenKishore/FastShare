cd729f28a46c870919b1db1c0a90e6c291885a43
PraveenKishore <praveenkishorepraveen@gmail.com>
2017-03-29 02:01:55 +0530

    Accept new connections

--- fastshare/Server.py
+++ fastshare/Server.py
@@ -1,7 +1,16 @@
 import socket
 from threading import Thread
+import sys
 
-SELF_SERVER_ADDR = ("127.0.0.1", 1947)
+defaultIP = "127.0.0.1"
+defaultPort = 1947
+
+if len(sys.argv) > 1:
+    defaultIP = sys.argv[1]
+if len(sys.argv) > 2:
+    defaultPort = int(sys.argv[2])
+
+SELF_SERVER_ADDR = (defaultIP, defaultPort)
 
 class  FastShareServer:
     def __init__(self):
@@ -13,20 +22,62 @@ class  FastShareServer:
     def startPeerServer(self, serverAddr = SELF_SERVER_ADDR, backlog=15):
         self.serverSock.bind(serverAddr)
         self.serverSock.listen(backlog)
+        self.serverSock.settimeout(2)
+        print("Listening to peer at: {}:{}".format(serverAddr[0], serverAddr[1]))
         while self.keepAlive:
-            client, addr = self.serverSock.accept()
-            self.listenThread = Thread(target=self.handlePeer, args=(client, addr))
+            try:
+                try:
+                    client, addr = self.serverSock.accept()
+                    #print(str((client, addr)))
+                    self.listenThread = Thread(target=self.handlePeer, args=(client, addr))
+                    self.listenThread.start()
+                except socket.timeout: pass
+            except KeyboardInterrupt:
+                self.stopPeerServer()
+                break
 
     def handlePeer(self, client, addr):
-        self.peersAddr.add(addr)
         print("Peer connected: {}".format(addr[0]))
+        self.peersAddr.add(addr)
+
+    def connectToPeer(self, addr):
+        self.peerSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.peerSock.connect(addr)
+        import time
+        time.sleep(2)
+        self.peerSock.shutdown(2)
+        self.peerSock.close()
 
     def displayAllPeers(self):
+        if len(self.peersAddr) == 0:
+            print("No peers connected so far!")
         for i in self.peersAddr:
             print(i)
 
+    def stopPeerServer(self):
+        print("Shutting down the peer server....")
+        self.keepAlive = False
+        if self.serverSock is not None:
+            try:
+                self.serverSock.shutdown(2)
+                self.serverSock.close()
+            except socket.error: pass
+
+    def handleInput(self):
+        while self.keepAlive:
+            try:
+                command = input()
+                if command is None or len(command) == 0: pass
+                if command == "display":
+                    self.displayAllPeers()
+                if command == "exit":
+                    self.stopPeerServer()
+            except EOFError as e:
+                break
 
 if __name__=="__main__":
     fastShareServer = FastShareServer()
+    Thread(target=fastShareServer.handleInput).start()
+    #fastShareServer.connectToPeer(("192.168.77.128", 1947))
     fastShareServer.startPeerServer()
-    fastShareServer.displayAllPeers()
+    #fastShareServer.displayAllPeers()
