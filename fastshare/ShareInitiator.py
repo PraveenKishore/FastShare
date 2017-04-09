7f47ed78802a2ebc6e59938f576a51e489e90e8d
PraveenKishore <praveenkishorepraveen@gmail.com>
2017-04-09 21:59:07 +0530

    Add Server

---
+++ fastshare/ShareInitiator.py
@@ -0,0 +1,39 @@
+import socket
+import threading
+
+class FSServer:
+    def __init__(self, port=1996):
+        self.port = port
+        self.keepAlive = True
+        self.availableClients = set()
+        self.serverSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        self.serverSock.bind(('0.0.0.0', self.port))
+        self.serverSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.serverSock.listen(50)
+        print("Server started at {}".format(socket.gethostbyname(socket.gethostname())))
+
+    def startListening(self):
+        while self.keepAlive:
+            client, addr = self.serverSock.accept()
+            threading.Thread(target=self.handleClient, args=(client, addr)).start()
+
+    def handleClient(self, client, addr):
+        print("Client discovered: {}".format(addr[0]))
+        self.availableClients.add(addr[0])
+        client.close()
+        pass
+
+    def handleInput(self):
+        while self.keepAlive:
+            command = input("").strip()
+            if command == "": continue
+            if command == "list":
+                if len(self.availableClients) == 0:
+                    print("No hosts discovered yet")
+                else:
+                    for host in self.availableClients: print(host)
+
+if __name__=="__main__":
+    fss = FSServer()
+    threading.Thread(target=fss.startListening).start()
+    threading.Thread(target=fss.handleInput).start()
